8e698732c531abb5b96375aaddc94986d08f61ee
Ezra-Mallo <ezra.mallo@gmail.com>
2022-10-13 16:12:57 +0100

    Task 0-preprocessor.

--- first_c.c
+++ first_c.c
@@ -1,20 +1,30 @@
 #include <stdio.h>
 #include <string.h>
 
-int main()
+
+/*
+ * This is the main functions
+ * For diplaying array
+*/
+
+int main(void)
 {
-   int a = 3;
-   int b = 4;	   
-   int i, j;
-   for (int i = 1; i <= a; i++) {
+	int a = 3;
+	int b = 4;
+	int i, j;
 
-   	   for (int j = 1; j <= b; j++) {
+	for (int i = 1; i <= a; i++)
 
-  	   	printf("%i, ", i);
-  	   	printf("%i", j);
-		printf("\n");
-//		printf("  Hello World\n");
+	{
+
+		for (int j = 1; j <= b; j++)
+
+		{
+			printf("%i, ", i);
+			printf("%i", j);
+			printf("\n");
+			/*printf("  Hello World\n"); */
+		}
 	}
-   }
-   return 0;
+	return (0);
 }

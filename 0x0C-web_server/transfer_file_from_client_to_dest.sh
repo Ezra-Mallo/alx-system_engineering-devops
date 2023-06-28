355f84e96a368db5bf2b188a78953d96ec9466b3
Ezra-Mallo <ezra.mallo@gmail.com>
2023-06-28 15:37:06 +0100

    Modified Task 1. Install nginx web server

--- 0x0C-web_server/transfer_file_from_client_to_dest.sh
+++ 0x0C-web_server/transfer_file_from_client_to_dest.sh
@@ -3,8 +3,8 @@
 
 if [ "$#" -lt "3" ];
   then
-  echo "Usage: ./transfer_file <Path_to_and_source_file> <UserName> <Remote_Host_IP> <Path_to_and_Dest_file>"
-  echo "Usage: ./transfer_file /alx/AirBnB_clone_v2/index.html  ubuntu  10.2.34.5.5  ~/var/www/html/"
+  #  echo "Usage: ./transfer_file <Path_to_and_source_file> <UserName> <Remote_Host_IP> <Path_to_and_Dest_file>"
+  echo "Usage: ./transfer_file </alx/index.html>  <ubuntu>  <10.2.34.5.5>  <~/var/www/html/>"
 else
   # Path_to_and_source_file
   argv1=$1  

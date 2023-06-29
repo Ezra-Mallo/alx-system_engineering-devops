bee86619c77d877fabfa71b6eb20dd2f1d3c17e5
Ezra-Mallo <ezra.mallo@gmail.com>
2023-06-29 14:16:33 +0100

    Some modifications on Task 5

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -5,34 +5,15 @@ package {'nginx':
   ensure => 'present',
 }
 
-# updates ubuntu
-exec {'update':
-  command  => 'sudo apt-get update',
-  provider => shell,
-}
-
-# install Nginx
 exec {'install':
-  command  => 'sudo apt-get install nginx -y',
+  command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
   provider => shell,
-}
-# OR: command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
 
-# create directory using shell
-exec {'make dir':
-  command  => 'sudo mkdir -p /var/www/nginx/html',
-  provider => shell,
 }
 
-# create Index page file
 exec {'Hello':
-  command  => 'echo "Hello World!" | sudo tee /var/www/nginx/html/index.html',
+  command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
   provider => shell,
-}
-
-# create 404 file
-file { '/etc/nginx/html/404.html':
-  content => "Ceci n'est pas une page\n",
 }
 
 exec {'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/blog.ehoneahobed.com\/;\\n\\t}/" /etc/nginx/sites-available/default':
@@ -43,3 +24,4 @@ exec {'run':
   command  => 'sudo service nginx restart',
   provider => shell,
 }
+

ac64c5b97419924d58af5f78bb4d8fb2d82e69a1
Ezra-Mallo <ezra.mallo@gmail.com>
2023-06-29 14:00:18 +0100

    Some modifications on Task 5

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -25,11 +25,17 @@ exec {'make dir':
   provider => shell,
 }
 
+# create Index page file
 exec {'Hello':
   command  => 'echo "Hello World!" | sudo tee /var/www/nginx/html/index.html',
   provider => shell,
 }
 
+# create /404 file
+exec {'Hello':
+  command  => 'echo "Ceci n\'est pas une page\n" | sudo tee /etc/nginx/html/404.html',
+  provider => shell,
+}
 
 exec {'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/blog.ehoneahobed.com\/;\\n\\t}/" /etc/nginx/sites-available/default':
   provider => shell,

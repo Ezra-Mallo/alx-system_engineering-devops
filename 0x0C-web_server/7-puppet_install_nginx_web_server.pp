ee0fab6389e24ac44b79f34ae70a15b067b5f8ed
Ezra-Mallo <ezra.mallo@gmail.com>
2023-06-29 13:01:15 +0100

    Some modifications

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -26,31 +26,31 @@ exec {'make dir':
 
 # create index file
 file { '/var/www/nginx/index.html':
-  ensure    => file,
-  path      => '/var/www/nginx/html/index.html',
-  content   => 'Hello World!',
-  mode      => '0644',
+  ensure  => file,
+  path    => '/var/www/nginx/html/index.html',
+  content => 'Hello World!',
+  mode    => '0644',
 }
 
 # create /404 file
 file { '/var/www/nginx/404.html':
-  ensure    => file,
-  path      => '/etc/nginx/html/';
-  content   => "Ceci n'est pas une page\n",
-  mode      => '0644',
+  ensure  => file,
+  path    => '/etc/nginx/html/',
+  content => "Ceci n'est pas une page\n",
+  mode    => '0644',
 }
 
 # assign value to the variable server_block 
-server_block = "server {
+server_block => "server {
         listen 80 default_server;
         listen [::]:80 default_server;
-        root   /var/www/nginx/html;        
+        root   /var/www/nginx/html;
         index index.html index.htm index.nginx-debian.html;
-        
+
         server_name _;
         location / {
                 try_files \$uri \$uri/ =404;
-        }        
+        }
         error_page 404 /404.html;
         location  /404.html {
             internal;
@@ -59,9 +59,17 @@ server_block = "server {
         if (\$request_filename ~ redirect_me){
             rewrite ^ https://www.youtube.com/watch?v=RgtNZGA6NYQ permanent;
         }
+}",
+
+
+
+# create /404 file
+file { '/var/www/nginx/404.html':
+  ensure  => file,
+  path    => '/etc/nginx/sites-available/default',
+  content => $server_block,
+  mode    => '0644',
 }
-"
-echo "$server_block" > /etc/nginx/sites-available/default
 
 # restart nginx
 exec {'run':
